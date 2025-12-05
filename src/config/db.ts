0189a474399a023e8b18a5dceb124570bd372136
shakib071 <shakibhasan071@gmail.com>
2025-12-05 15:40:03 +0600

    added booking db table

--- src/config/db.ts
+++ src/config/db.ts
@@ -30,14 +30,33 @@ const initDB = async() => {
             vehicle_name TEXT NOT NULL,
             type VARCHAR(20) CHECK(type IN ('car','bike','van','SUV')),
             registration_number VARCHAR(200) NOT NULL UNIQUE,
-            daily_rent_price NUMERIC(15,3) NOT NULL CHECK(daily_rent_price > 0),
+            daily_rent_price NUMERIC(12,3) NOT NULL CHECK(daily_rent_price > 0),
             availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked'))
 
         )
 
     `);
 
-    
+
+    // Bookings DB table 
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS Bookings(
+            id SERIAL PRIMARY KEY,
+            customer_id INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+            vehicle_id INT NOT NULL REFERENCES Vehicles(id) ON DELETE CASCADE,
+            rent_start_date DATE NOT NULL,
+            rent_end_date DATE NOT NULL CHECK(rent_end_date > rent_start_date),
+            total_price NUMERIC(12,3) NOT NULL CHECK(total_price > 0),
+            status VARCHAR(20) NOT NULL CHECK(status IN ('active','cancelled','returned'))
+        )
+        
+    `);
+
+
+
+
+
     
 }
 
